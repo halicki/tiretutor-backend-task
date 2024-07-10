3d0f2c962caa60f33d651dbe6ff5bc6b3e696877
Arkadiusz Halicki <halicki.arkadiusz@gmail.com>
2024-07-10 21:56:10 +0200

    Setup bootstrap

--- starwars/views.py
+++ starwars/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("Hello, world. Returning from basic view")
+    return render(request, "starwars/index.html")

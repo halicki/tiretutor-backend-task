03762ad10d29439736c00657a33302cf7505bf46
Arkadiusz Halicki <halicki.arkadiusz@gmail.com>
2024-07-15 10:09:05 +0200

    Add comments

--- starwars/views.py
+++ starwars/views.py
@@ -24,7 +24,10 @@ class CollectionDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        # Get the selected columns from the query string
         group = self.request.GET.getlist("group")
+
+        # Create a list of dictionaries with the column names and whether they are active
         columns = [
             {"name": c, "active": c in group}
             for c in PersonTable.base_columns.keys()
@@ -32,14 +35,17 @@ class CollectionDetailView(DetailView):
         ]
         context["columns"] = columns
 
+        # Get the data from the collection
         data = self.object.get_data()
         if group:
+            # Group the data by the selected columns
             df_grouped = data.aggregate(key=group, aggregation=len, field="count")
             table = PersonTableWithCount(
                 df_grouped.dicts(),
                 exclude=[c["name"] for c in columns if not c["active"]],
             )
         else:
+            # Show the data as is
             table = PersonTable(data.dicts())
             table.paginate(page=self.request.GET.get("page", 1), per_page=10)
 
